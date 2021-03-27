2bae486058f651c756316b775890d8e315b49bed
Valerio Narcisi <valerio.narcisi@gmail.com>
2021-03-27 16:30:50 +0100

    added getSearched selectos

--- src/containers/SearchContainer/selectors.ts
+++ src/containers/SearchContainer/selectors.ts
@@ -1,13 +1,18 @@
 import { createSelector } from "@reduxjs/toolkit"
-import { IState } from "../../app/store"
+import { RootState } from "../../app/store"
 import { featureKey } from "./slice"
 
 export const getPersonas = createSelector(
-    (state: IState) => state[featureKey],
+    (state: RootState) => state[featureKey],
     (personState) => Object.values(personState.byIds || {})
 )
 
 export const getDirector = createSelector(
     getPersonas,
     (personas) => personas.filter(p => p.known_for_department === 'Directing')
+)
+
+export const getSearched = createSelector(
+    (state: RootState) => state[featureKey],
+    (state) => state.searched
 )
