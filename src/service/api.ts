cfb5fb0c53ee156058b64119a93c2adb804dadb5
Valerio Narcisi <valerio.narcisi@gmail.com>
2021-03-30 22:18:05 +0200

    moved axios to ajax rxjs

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,8 +1,11 @@
-import axios from "axios";
 import { normalize, schema } from "normalizr";
 import { Movie } from "../containers/DirectorContainer/model";
 import { Person } from "../containers/SearchContainer/model";
+import { of, Observable } from "rxjs";
+import { map, catchError, tap } from "rxjs/operators";
+import { ajax } from 'rxjs/ajax';
 import { PersonSucceededPayload } from "../containers/SearchContainer/slice";
+import { DirectorSucceeededPayload } from "../containers/DirectorContainer/slice";
 
 export const API_KEY = 'a90d99d87d52ef1f55e06af62b50fadc';
 export const IMAGES_URL = 'https://image.tmdb.org/t/p/w500';
@@ -11,55 +14,52 @@ export const VERSION = '3';
 
 const SCHEMA_FEATURE_KEY = 'person';
 
-
 export const searchPerson = (query: string, page = 1): string => `${BASE_URL}${VERSION}/search/person?api_key=${API_KEY}&query=${query}&page=${page}`
 
-export const fetchPerson = async (person: string): Promise<PersonSucceededPayload> => {
-    try {
-        const { data } = await axios.get(searchPerson(person));
-        const normalized = normalize<
-            any,
-            {
-                [SCHEMA_FEATURE_KEY]: { [key: string]: Person }
+export const ajaxApiPerson = (person: string): Observable<PersonSucceededPayload> => {
+    return ajax.getJSON<PersonSucceededPayload>(searchPerson(person)).pipe(
+        map((data: any) => {
+            const normalized = normalize<
+                any,
+                {
+                    [SCHEMA_FEATURE_KEY]: { [key: string]: Person }
+                }
+            >(data.results, [new schema.Entity(SCHEMA_FEATURE_KEY)])
+
+            return {
+                byIds: normalized.entities[SCHEMA_FEATURE_KEY],
+                allIds: normalized.result,
+                currentPage: data.page,
+                totalPages: data.total_pages,
+                totalResults: data.total_results,
             }
-        >(data.results, [new schema.Entity(SCHEMA_FEATURE_KEY)])
-
-        return {
-            byIds: normalized.entities[SCHEMA_FEATURE_KEY],
-            allIds: normalized.result,
-            currentPage: data.page,
-            totalPages: data.total_pages,
-            totalResults: data.total_results,
-        }
-
-    } catch (e) {
-        throw new Error(e)
-    }
+        }),
+        catchError(error => {
+            return of(error);
+        })
+    )
 }
 
 export const searchPersonDetail = (id: string): string => `${BASE_URL}${VERSION}/person/${id}/combined_credits?api_key=${API_KEY}`
 const SCHEMA_DETAIL_FEATURE_KEY = 'personDetail';
 
-export const fetchPersonDetail = async (id: string) => {
-    try {
-        const { data } = await axios.get(searchPersonDetail(id));
-        const normalized = normalize<
-            any,
-            {
-                [SCHEMA_DETAIL_FEATURE_KEY]: { [key: string]: Movie }
+export const ajaxApiPersonDetail = (id: string): Observable<DirectorSucceeededPayload> => {
+    return ajax.getJSON<DirectorSucceeededPayload>(searchPersonDetail(id)).pipe(
+        map((data: any) => {
+            const normalized = normalize<
+                any,
+                {
+                    [SCHEMA_DETAIL_FEATURE_KEY]: { [key: string]: Movie }
+                }
+            >(data.cast, [new schema.Entity(SCHEMA_DETAIL_FEATURE_KEY)])
+
+            return {
+                byIds: normalized.entities[SCHEMA_DETAIL_FEATURE_KEY],
+                allIds: normalized.result,
             }
-        >(data.cast, [new schema.Entity(SCHEMA_DETAIL_FEATURE_KEY)])
-
-        console.log(data, {
-            byIds: normalized.entities[SCHEMA_DETAIL_FEATURE_KEY],
-            allIds: normalized.result,
+        }),
+        catchError(error => {
+            return of(error);
         })
-        return {
-            byIds: normalized.entities[SCHEMA_DETAIL_FEATURE_KEY],
-            allIds: normalized.result,
-        }
-
-    } catch (e) {
-        throw new Error(e)
-    }
+    )
 }
